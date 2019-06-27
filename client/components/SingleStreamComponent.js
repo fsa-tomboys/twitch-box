371c179c4c0075b579ede84fef27f27aeef4f57b
Olga Shiryaeva <oshiryaeva@protonmail.com>
2019-06-26 19:28:46 -0500

    made basic single stream component, with menu overlay on top that appears on hover

---
+++ client/components/SingleStreamComponent.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react'
+import ReactTwitchEmbedVideo from 'react-twitch-embed-video'
+
+class SingleStreamComponent extends Component {
+  render() {
+    return (
+      <div className="single-stream-outer">
+        <div className="single-stream-overlay-menu">
+          <span className="single-stream-overlay-links">test</span>
+        </div>
+        <iframe
+          src={`https://player.twitch.tv/?allowfullscreen&channel=${
+            this.props.name
+          }`}
+          width="640"
+          height="390"
+          frameBorder="0"
+          scrolling="no"
+          allow="autoplay; fullscreen"
+          allowFullScreen=""
+        />
+      </div>
+    )
+  }
+}
+
+export default SingleStreamComponent
--- client/components/Test.js
+++ client/components/Test.js
@@ -1,31 +1,59 @@
-import React from 'react'
+import React, {Component} from 'react'
 import ReactTwitchEmbedVideo from 'react-twitch-embed-video'
 import {Grid} from 'semantic-ui-react'
+import equal from 'fast-deep-equal'
+import SingleStreamComponent from './SingleStreamComponent'
 
-export const Test = () => {
-  return (
-    <div>
-      <Grid>
-        <Grid.Row>
-          <Grid.Column width={8}>
-            <ReactTwitchEmbedVideo channel="talk2megooseman" />
-          </Grid.Column>
-          <Grid.Column width={8}>
-            <ReactTwitchEmbedVideo channel="talk2megooseman" />
-          </Grid.Column>
-        </Grid.Row>
+export class Test extends Component {
+  constructor() {
+    super()
+    this.state = {
+      width: window.innerHeight,
+      height: window.innerHeight,
+      testArray: ['orb', 'maxgrosshandler']
+    }
+    this.updateDimensions = this.updateDimensions.bind(this)
+  }
 
-        <Grid.Row>
-          <Grid.Column width={8}>
-            <ReactTwitchEmbedVideo channel="talk2megooseman" />
-          </Grid.Column>
-          <Grid.Column width={8}>
-            <ReactTwitchEmbedVideo channel="talk2megooseman" />
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-    </div>
-  )
+  updateDimensions() {
+    console.log(this.state)
+    this.setState({width: window.innerWidth, height: window.innerHeight})
+    console.log(this.state)
+  }
+
+  componentDidMount() {
+    this.updateDimensions()
+    window.addEventListener('resize', () => {
+      this.updateDimensions()
+      // console.log(this.state.width)
+      // console.log(this.state.height)
+    })
+  }
+
+  // componentWillMount() {
+  //   this.updateDimensions();
+  //   console.log(this.state.width)
+  //   console.log(this.state.height)
+  // }
+
+  //   componentWillUnmount() {
+  //     window.removeEventListener("resize",  this.updateDimensions())
+
+  //     // console.log(this.state)
+  // }
+
+  render() {
+    // console.log('RENDER:', typeof this.state.width)
+    // let newWidth = String(this.state.width)
+    // let newHeight = String(this.state.height)
+    return (
+      <div className="main-layout-container">
+        {this.state.testArray.map(element => (
+          <SingleStreamComponent name={element} />
+        ))}
+      </div>
+    )
+  }
 }
 
 export default Test
