1e649aa45475f05706d811efdbc696dd7ddd84bd
Olga Shiryaeva <oshiryaeva@protonmail.com>
2019-06-27 13:54:07 -0500

    added basic sidebar component (no styling or links yet) and changed layout (css) for 4 streams so they are placed correctly

--- client/components/MultiStream.js
+++ client/components/MultiStream.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import SingleStreamComponent from './SingleStreamComponent'
 import {Chat} from './chat'
+import MultistreamSidebar from './MultistreamSidebar'
 
 export class MultiStream extends Component {
   constructor() {
@@ -12,10 +13,13 @@ export class MultiStream extends Component {
   render() {
     return (
       <div className="main-layout-container">
-        {this.props.location.state.testArray.map(element => (
-          <SingleStreamComponent name={element} />
-        ))}
-        <Chat testArray={this.props.location.state.testArray} />
+        <MultistreamSidebar />
+        <div className="all-streams-container">
+          {this.props.location.state.testArray.map(element => (
+            <SingleStreamComponent name={element} />
+          ))}
+          <Chat testArray={this.props.location.state.testArray} />
+        </div>
       </div>
     )
   }
