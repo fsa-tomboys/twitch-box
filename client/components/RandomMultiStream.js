73ff97462fbf1efc40e276a651944082869aaea8
Olga Shiryaeva <oshiryaeva@protonmail.com>
2019-07-01 21:02:37 -0500

    changes to featured component2

---
+++ client/components/RandomMultiStream.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react'
+
+class RandomMultistream extends Component {
+  render() {
+    let width = this.props.height
+    let height = this.props.width
+
+    return (
+      <div className="random-single-stream-inner">
+        <iframe
+          src={`https://player.twitch.tv/?allowfullscreen&channel=${
+            this.props.name
+          }`}
+          width={width}
+          height={height}
+          frameBorder="0"
+          scrolling="no"
+          allow="autoplay; fullscreen"
+          allowFullScreen=""
+          className=""
+        />
+      </div>
+    )
+  }
+}
+
+export default RandomMultistream
