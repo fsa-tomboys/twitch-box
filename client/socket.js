a2478d0900cc00990eaf4d7169045b9082d433ec
MaxGrosshandler <maxgrosshandler@gmail.com>
2019-07-03 11:40:10 -0500

    got some proof of concept work done with sockets

--- client/socket.js
+++ client/socket.js
@@ -5,5 +5,7 @@ const socket = io(window.location.origin)
 socket.on('connect', () => {
   console.log('Connected!')
 })
-
+socket.on('clicked', () => {
+  console.log('item was clicked')
+})
 export default socket
--- server/socket/index.js
+++ server/socket/index.js
@@ -1,7 +1,9 @@
 module.exports = io => {
   io.on('connection', socket => {
     console.log(`A socket connection to the server has been made: ${socket.id}`)
-
+    socket.on('onClick', () => {
+      io.emit('clicked')
+    })
     socket.on('disconnect', () => {
       console.log(`Connection ${socket.id} has left the building`)
     })
