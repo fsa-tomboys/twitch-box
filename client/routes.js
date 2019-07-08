d00bb95715348208a6c864eff6e5d54ac9e439e0
Olga Shiryaeva <oshiryaeva@protonmail.com>
2019-07-08 11:47:09 -0500

    added navbar to ViewClipsComponent, added route for '/'

--- client/routes.js
+++ client/routes.js
@@ -39,6 +39,7 @@ class Routes extends Component {
         <Route path="/customize" component={Customize} />
         <Route path="/featured" component={Featured} />
         <Route path="/chat" component={Chat} />
+        <Route path="/" component={Featured} />
         {/* <Route path="/list" component={List} /> */}
         {this.props.location.search.includes('name') && (
           <Route path="/widget" component={Widget} />
