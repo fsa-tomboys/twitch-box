00e3129c63ddd5d282cc8618faeeeac32f5d135f
MaxGrosshandler <maxgrosshandler@gmail.com>
2019-07-08 10:58:23 -0500

    more route fixes

--- client/routes.js
+++ client/routes.js
@@ -39,18 +39,12 @@ class Routes extends Component {
         <Route path="/customize" component={Customize} />
         <Route path="/featured" component={Featured} />
         <Route path="/chat" component={Chat} />
-        <Route path="/" component={Featured} />
-        {/* <Route path="/list" component={List} /> */}
         {this.props.location.search.includes('name') && (
           <Route path="/widget" component={Widget} />
         )}
+        <Route path="/" component={Featured} />
+        {/* <Route path="/list" component={List} /> */}
 
-        {isLoggedIn && (
-          <Switch>
-            {/* Routes placed here are only available after logging in */}
-            <Route path="/home" component={UserHome} />
-          </Switch>
-        )}
         {/* Displays our Login component as a fallback */}
       </Switch>
     )
