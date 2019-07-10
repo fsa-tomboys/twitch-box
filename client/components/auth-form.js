2c010090315963931cce78d198ee3620e6d952b2
MaxGrosshandler <maxgrosshandler@gmail.com>
2019-07-10 09:56:16 -0500

    started work on login and time

--- client/components/auth-form.js
+++ client/components/auth-form.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {connect} from 'react-redux'
 import PropTypes from 'prop-types'
 import {auth} from '../store'
-
+import Navbar from './navbar'
 /**
  * COMPONENT
  */
@@ -10,23 +10,27 @@ const AuthForm = props => {
   const {name, displayName, handleSubmit, error} = props
 
   return (
-    <div className="login-link-twitch">
-      <div className="login-signup-box">
-        <span className="welcome-text-login">
-          <h2>Welcome to Twitch Box!</h2>
-        </span>
-        <a className="login-link-twitch-text-box" href="/auth/twitch">
-          <span className="login-link-twitch-text-itself">
-            <span>Log in with Twitch</span>
+    <div>
+      <Navbar pos="navbar" />
+
+      <div className="login-link-twitch">
+        <div className="login-signup-box">
+          <span className="welcome-text-login">
+            <h2>Welcome to Twitch Box!</h2>
           </span>
-        </a>
-        <span className="sign-up-question">
-          Need to create Twitch account?
-          <a className="sign-up-twitch" href="https://www.twitch.tv/signup">
-            {' '}
-            Sign up here
+          <a className="login-link-twitch-text-box" href="/auth/twitch">
+            <span className="login-link-twitch-text-itself">
+              <span>Log in with Twitch</span>
+            </span>
           </a>
-        </span>
+          <span className="sign-up-question">
+            Need to create Twitch account?
+            <a className="sign-up-twitch" href="https://www.twitch.tv/signup">
+              {' '}
+              Sign up here
+            </a>
+          </span>
+        </div>
       </div>
     </div>
   )
