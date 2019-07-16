68d40e97508998a2273eab221122b936f6481e9d
MaxGrosshandler <maxgrosshandler@gmail.com>
2019-07-16 13:51:55 -0500

    added some more comments

--- client/components/ViewClipsComponent.js
+++ client/components/ViewClipsComponent.js
@@ -4,7 +4,7 @@ import Navbar from './navbar'
 // import {Chat} from './chat'
 // import MultistreamSidebar from './MultistreamSidebar'
 import queryString from 'query-string'
-
+//This component allows for a user to view the multistream clip you have created
 export class ViewClipsComponent extends Component {
   constructor() {
     super()
@@ -12,7 +12,9 @@ export class ViewClipsComponent extends Component {
       clipsArray: []
     }
   }
+
   componentDidMount() {
+    // Takes in a array of clips either from the querystring or from props
     let queryStuff = queryString.parse(this.props.match.params.list)
     if (queryStuff.list === undefined) {
       queryStuff = queryString.parse(this.props.location.search)
@@ -24,6 +26,7 @@ export class ViewClipsComponent extends Component {
   }
 
   render() {
+    //Renders the clips in Single Clip Components
     let length = this.state.clipsArray.length
     return (
       <div>
